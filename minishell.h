a65a4e4f2d1f516311754a846591b59476b902cf
Naoufal Bougrine <nbougrin@e1r7p14.1337.ma>
2025-04-17 12:05:17 +0100

    sgv_handel

--- minishell.h
+++ minishell.h
@@ -24,8 +24,8 @@ typedef struct s_token {
     char *content;
     t_token_type type;
     int index;
+	char *in;
 	char *original;
-	char *in_add;
     struct s_token *next;
 } t_token;
 // ------------------------------------
@@ -36,17 +36,13 @@ int main();
 void lexer_2(t_token **tokens, char *input, int *i, int *index);
 void	ft_word(t_token **tokens, char *input, int *i, int *index);
 void	ft_quote(t_token **tokens, char *input, int *i, int *index);
-char	*parse_word_with_quotes(char *input, int *i, t_token **token);
-char	*ft_strjoin(char const *s1, char const *s2);
-// static char	*join_and_free(char *s1, char *s2);
-void	ft_exit(t_token **token);
 //                **  token_tools  **
 void	add_token(t_token **lst, char *content, t_token_type type, int index);
 t_token	*new_token(char *content, t_token_type type);
 char	*substr(char *s, int start, int len);
-void	ft_lstclear(t_token **lst);
 
 // ---------------general tools -----------------
 char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
