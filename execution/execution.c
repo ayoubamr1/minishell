2bd4c8f7582a8d8c28395e40eaa63ef5d332ce2f
Ayoub Ameur <ayoubameur95@gmail.com>
2025-05-06 17:14:14 +0100

    beggin execution

--- execution/execution.c
+++ execution/execution.c
@@ -6,19 +6,21 @@
 /*   By: ayameur <ayameur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 15:33:02 by ayameur           #+#    #+#             */
-/*   Updated: 2025/05/06 15:55:21 by ayameur          ###   ########.fr       */
+/*   Updated: 2025/05/06 17:13:50 by ayameur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	execute_commande(t_cmd *cmd, t_env **env)
+void	execute_commande(t_cmd *cmd, t_env *env)
 {
 	t_cmd	*cur;
 	int		pipe_fd[2];
 	pid_t	pid;
+	int		prev_read;
 	
 	cur = cmd;
+	prev_read = 0;
 	while (cur)
 	{
 		if (cur->next)
@@ -38,9 +40,19 @@ void	execute_commande(t_cmd *cmd, t_env **env)
 			{
 				if (cur->in != 0)
 					dup2(cur->in, 0);
-					
+				else if (prev_read)
+					dup2(prev_read, 0);
+				if (cur->out != 1)
+					dup2(cur->out, 1);
+				else if (cur->next)
+					dup2(pipe_fd[1], 1);
+				if (execve(cur->cmd[0], cur->cmd, env->content) == -1)
+				{
+					perror("exec failled\n");
+					exit(EXIT_FAILURE);
+				}
 			}
 		}
+		cur = cur->next;
 	}
-	
 }
