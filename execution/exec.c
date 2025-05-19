55d45e4d0039263988591ba740b738d32685b933
Ayoub Ameur <ayoubameur95@gmail.com>
2025-05-19 20:20:12 +0100

    fixe multiple commande with builtins

--- execution/exec.c
+++ execution/exec.c
@@ -6,7 +6,7 @@
 /*   By: ayameur <ayameur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:01:48 by ayameur           #+#    #+#             */
-/*   Updated: 2025/05/19 19:28:58 by ayameur          ###   ########.fr       */
+/*   Updated: 2025/05/19 20:02:00 by ayameur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	ft_check_child(t_cmd *cmd, int read_fd, int write_fd, t_shell *main)
 		exit(1);
 	}
 	printf("is Not_builtins : %s\n", cmd->cmd[0]);
-	if (execve(cmd->cmd[0], cmd->cmd, env_in_2D(main)) == -1) /// update with my env
+	if (execve(cmd->cmd[0], cmd->cmd, env_in_2D(main)) == -1)
 	{
 		perror("execve");
 		exit(1);
